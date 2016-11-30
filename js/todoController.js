37709dc1e84b4edbfebaf069090cab526eb8330c
Michael Cruz <michael.cruz@uwalumni.com>
2016-11-30 12:08:45 -0800

    initial commit

--- js/todoController.js
+++ js/todoController.js
@@ -1,10 +1,25 @@
 (function() {
   'use strict';
     angular.module('todoApp')
-    .controller('TodoController', TodoController)
+    .controller('TodoController', TodoController);
 
     function TodoController(){
       var vm = this
-      vm.todo = ''
-    }
+      vm.todo = {thing: '', done: false}
+      vm.add = addTodo
+
+
+      vm.all = [
+        {thing: 'get a mint from frontlines', done: false},
+        {thing: 'eat mint', done: false},
+        {thing: 'code', done: false},
+        {thing: '...', done: false},
+        {thing: 'profit', done: false}
+      ];
+
+      function addTodo() {
+        vm.all.push(vm.todo);
+        vm.todo = {thing: '', done: false};
+      }
+    };
 }());
